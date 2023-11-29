3adbeed8f1e70174b4d37435b38ef6c1630944e8
Sadiq Ahmed <offsznahmed@Sadiqs-MacBook-Pro.local>
2023-11-29 11:42:38 -0800

    Finished make home page layout resizeable

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
-import Welcome from "../../images/Welcome.png";
-import Logo from "../../images/Logo.png";
-import LashesBy from "../../images/LashesBy.png";
 
-import Hours from "../../images/Hours.png";
-import Location from "../../images/Location.png";
-import Contact from "../../images/Contact.png";
+import Welcome from "../../images/Welcome.png";
+
+import Lashes from "../../images/Lashes.png";
+
+import HrConLoc from "../../images/HrConLoc.png";
 
 import Policies from "../../images/Policies.png";
 
@@ -41,18 +37,11 @@ function Home() {
             </div>
 
             <div className="HP-Logo-Div">
-                <img src={Logo} alt="Logo" className="HP-Logo-Img"></img>
-                <img src={LashesBy} alt="LashesBy" className="HP-LashesBy-Img"></img>
+                <img src={Lashes} alt="Lashes" className="HP-Lashes-Img"></img>
             </div>
 
             <div className="HP-Contact-Div">
-                <div className="HP-Hr-Loc-Container">
-                    <img src={Hours} alt="Hours" className="HP-Hours-Img"></img>
-                    <img src={Location} alt="Location" className="HP-Location-Img"></img>
-                </div>
-                <div className="HP-Contact-Container">
-                    <img src={Contact} alt="Contact" className="HP-Contact-Img"></img>
-                </div>
+                <img src={HrConLoc} alt="HrConLoc" className="HP-HrConLoc-Img"></img>
             </div>
 
             <div className="HP-Policies-Div">
